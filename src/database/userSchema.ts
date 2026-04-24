8469e665299ae3387d37a7195fdf145153fcc929
uzairkhanworks <uzair.khan@boonindia.ai>
2026-04-24 11:47:55 +0530

    added user data model.

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,1 +1,14 @@
 // user data model
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    age: Number,
+    
+})
+
+const User = mongoose.model('User', userSchema);
+export default User;
