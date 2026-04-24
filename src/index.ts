22c9f06ef0a0987a645ca990d4c2281c733cc6ae
uzairkhanworks <uzair.khan@boonindia.ai>
2026-04-24 11:29:06 +0530

    added a user Route.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 // main index file.
 
 import express from "express";
+import userRouter from "./routes/userRoutes";
+const app=express();
+app.use(express.json());
+
+app.use("/user", userRouter);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,8 @@ import express from "express";
 
 const userRouter=express.Router();
 
-
+userRouter.get("/", async(req,res)=> {
+// cahce logic
+});
 
 export default userRouter;
